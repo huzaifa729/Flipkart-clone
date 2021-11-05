a7c04110ee795040c1e27dd8d812bc5b53225497
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2021-11-05 04:07:47 -0700

    init

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,16 @@ import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import HelpIcon from '@mui/icons-material/Help';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AssistantSharpIcon from '@mui/icons-material/AssistantSharp';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import AttachEmailIcon from '@mui/icons-material/AttachEmail';
 
 function Header() {
     return (
@@ -32,11 +42,68 @@ function Header() {
                   <div className="login">
                       <a href="/">
                       <button className="btn">Login</button>
-                      </a>
+                   </a>
+                      <div className="bint">
+                          <div className="sint">
+                             <div className="op">
+                            <span className="ocon"><AccountCircleIcon fontSize="small"/></span>
+                            <span className="writens">Profile</span>
+                            </div>
+
+                              <div className="op">
+                                  <span className="ocon"><AssistantSharpIcon fontSize="small"/></span>
+                                  <span className="writens">Flipkart Plus Zone</span>
+                              </div>
+
+                              <div className="op">
+                                  <span className="ocon"><FavoriteBorderIcon fontSize="small"/></span>
+                                  <span className="writens">Wishlist</span>
+                              </div>
+
+                              <div className="op">
+                                  <span className="ocon"><AccountBalanceWalletIcon fontSize="small"/></span>
+                                  <span className="writens">Gift Card</span>
+                              </div>
+
+                              <div className="op">
+                                  <span className="ocon"><AttachEmailIcon fontSize="small"/></span>
+                                  <span className="writens">Email</span>
+                              </div>
+                          </div>
+                      </div>
+                   
                   </div>
 
                   <div className="dol">
-                      More<KeyboardArrowDownIcon fontSize="small" className="down-icon"/>
+                    More<KeyboardArrowDownIcon fontSize="small" className="down-iscon"/>
+                      <div className="bint">
+                          <div className="sint">
+                              <div className="op">
+                                  <span className="ocon"><NotificationsIcon fontSize="small"/></span>
+                                  <span className="writens">Notifications Preference</span>
+                              </div>
+
+                              <div className="op">
+                                  <span className="ocon"><CardGiftcardIcon fontSize="small"/></span>
+                                  <span className="writens">Sell on Flipkart</span>
+                              </div>
+
+                              <div className="op">
+                                  <span className="ocon"><HelpIcon fontSize="small"/></span>
+                                  <span className="writens">24*7 Customer Care</span>
+                              </div>
+
+                              <div className="op">
+                                  <span className="ocon"><TrendingUpIcon  fontSize="small"/></span>
+                                  <span className="writens">Advertise</span>
+                              </div>
+
+                              <div className="op">
+                                  <span className="ocon"><FileDownloadIcon fontSize="small"/></span>
+                                  <span className="writens">Download App</span>
+                              </div>
+                          </div>
+                      </div>
                   </div>
 
 
