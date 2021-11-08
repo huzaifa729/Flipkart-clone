af6a1bed9a5d8fc5ba066e6ba5f52e9154607de2
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2021-11-08 02:59:31 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Header2 from './Header2';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Header2/>
     </div>
   );
 }
---
+++ src/Header2.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import './Header2.css'
+function Header2() {
+    return (
+        
+          <div className='All-Navse'>
+
+                <div className='all-photoes'>
+                    <a href="/">
+                    <div className='mage'>
+                        <img src='https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100'alt="1"/>
+                    </div>
+                  <div className='heading'>Top Offers</div>
+                    </a>
+                </div>
+
+
+                <div className='all-photoes'>
+                    <a href="/">
+                    <div className='mage'>
+                        <img src='	https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100'alt="2"/>
+                    </div>
+                  <div className='heading'>Grocery</div>
+                    </a>
+                </div>
+
+                <div className='all-photoes'>
+                    <a href="/">
+                    <div className='mage'>
+                        <img src='	https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100'alt="3"/>
+                    </div>
+                  <div className='heading'>Mobiles</div>
+                    </a>
+                </div>
+
+                <div className='all-photoes'>
+                    <a href="/">
+                    <div className='mage'>
+                        <img src='https://rukminim1.flixcart.com/flap/128/128/image/82b3ca5fb2301045.png?q=100'alt="4"/>
+                    </div>
+                  <div className='heading'>Fashion</div>
+                    </a>
+                </div>
+
+                <div className='all-photoes'>
+                    <a href="/">
+                    <div className='mage'>
+                        <img src='https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100'alt="5"/>
+                    </div>
+                  <div className='heading'>Electronics</div>
+                    </a>
+                </div>
+
+                <div className='all-photoes'>
+                    <a href="/">
+                    <div className='mage'>
+                        <img src='https://rukminim1.flixcart.com/flap/128/128/image/ab7e2b022a4587dd.jpg?q=100'alt="6"/>
+                    </div>
+                  <div className='heading'>Home</div>
+                    </a>
+                </div>
+
+                <div className='all-photoes'>
+                    <a href="/">
+                    <div className='mage'>
+                        <img src='https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100'alt="7"/>
+                    </div>
+                  <div className='heading'>Appliances</div>
+                    </a>
+                </div>
+
+                <div className='all-photoes'>
+                    <a href="/">
+                    <div className='mage'>
+                        <img src='https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100'alt="8"/>
+                    </div>
+                  <div className='heading'>Travel</div>
+                    </a>
+                </div>
+
+                <div className='all-photoes'>
+                    <a href="/">
+                    <div className='mage'>
+                        <img src='https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100'alt="9"/>
+                    </div>
+                  <div className='heading'>Beauty,Toys&More</div>
+                    </a>
+                </div>
+   
+          </div>
+      
+    )
+}
+
+export default Header2
