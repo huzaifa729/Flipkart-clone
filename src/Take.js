58195c308f5ce91be85b1c4b4de7e0e3e49ea093
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2021-12-06 21:52:26 -0800

    init

--- src/Take.js
+++ src/Take.js
@@ -3,72 +3,147 @@ import './Take.css'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import BoltIcon from '@mui/icons-material/Bolt';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
 
 function Take() {
     return (
-       <div className='hum'>
+        <div className='hum'>
 
-               <div className='gyu'>
+            <div className='gyu'>
 
-                   <div className='dut'>
-                       <div className='buk'>
-                           <div className='imge'>
-                               <img src="https://rukminim1.flixcart.com/image/800/960/k2jbyq80/shoe/f/u/y/cg-248-11-campus-blu-sky-original-imafhvb9gzrugdz3.jpeg?q=50"alt="lo"/>
-                           </div>
-                           <div className='btkns'>
-                               <button className="btn1">
-                                   <a href='/'>
-                               <ShoppingCartIcon className='oin' fontSize='small'/>ADD TO CART
-                               </a>
-                               </button>
+                <div className='dut'>
+                    <div className='buk'>
+                        <div className='imge'>
+                            <img src="https://rukminim1.flixcart.com/image/800/960/k2jbyq80/shoe/f/u/y/cg-248-11-campus-blu-sky-original-imafhvb9gzrugdz3.jpeg?q=50" alt="lo" />
+                        </div>
+                        <div className='btkns'>
+                            <button className="btn1">
+                                <a href='/'>
+                                    <ShoppingCartIcon className='oin' fontSize='small' />ADD TO CART
+                                </a>
+                            </button>
 
-                               <button className="btn2">
-                               <a href='/'>
-                               <BoltIcon className='oin' fontSize='small'/>BY NOW
-                               </a>
-                               </button>
-                           </div>
-                       </div>
-                   </div>
+                            <button className="btn2">
+                                <a href='/'>
+                                    <BoltIcon className='oin' fontSize='small' />BY NOW
+                                </a>
+                            </button>
+                        </div>
+                    </div>
+                </div>
 
-                   <div className='btyu'>
-                       <div className='ko'>
-                          <div className='vindesl'>
-                              <span className='oon'>CAMPUS</span>
-                              <span className='son'>ROYCE-2 Running Shoes For Men  (Blue)</span>
-                          </div>
+                <div className='btyu'>
+                    <div className='ko'>
+                        <div className='vindesl'>
+                            <span className='oon'>CAMPUS</span>
+                            <span className='son'>ROYCE-2 Running Shoes For Men  (Blue)</span>
+                        </div>
 
-                          <div className='spcal'>
-                          Special price
-                       </div>
+                        <div className='spcal'>
+                            Special price
+                        </div>
 
-                       <div className='prices'>
-                           <span className='on1'>₹965</span>
-                           <span className='on2'><del>₹1,049</del></span>
-                           <span className='on3'>8% off</span>
-                       </div>
+                        <div className='prices'>
+                            <span className='on1'>₹965</span>
+                            <span className='on2'><del>₹1,049</del></span>
+                            <span className='on3'>8% off</span>
+                        </div>
 
-                          <div className='str'>
-                             <div className='star'>4.2<StarBorderIcon className='i-st' fontSize='small'/></div>
-                             
-                             <div className='names'>
-                             8,207 ratings and 1,132 reviews
-                             </div>
+                        <div className='str'>
+                            <div className='star'>4.2<StarBorderIcon className='i-st' fontSize='small' /></div>
 
-                             <div className='flpk'>
-                                 <img src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png"alt='lo'/>
-                             </div>
-                          </div>
+                            <div className='names'>
+                                8,207 ratings and 1,132 reviews
+                            </div>
 
-                          <div className='biknm'>
-                              <div className='head'>Available offers</div>
-                          </div>
-                       </div>
-                   </div>
+                            <div className='flpk'>
+                                <img src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png" alt='lo' />
+                            </div>
+                        </div>
 
-               </div>
+                        <div className='biknm'>
+                            <div className='head'>Available offers</div>
+                            <div className="Akl">
+                                <div className='omn'>
+                                    <span className='pan'><LocalOfferIcon fontSize='small' /></span>
+                                    <span className='darr'>Special Price</span>
+                                    <span className='war'>Get extra 3% off (price inclusive of discount)</span>
+                                    <span className='hrf'><a href='/'>T&C</a></span>
+                                </div>
 
-       </div>
+                                <div className='omn'>
+                                    <span className='pan'><LocalOfferIcon fontSize='small' /></span>
+                                    <span className='darr'>Special Price</span>
+                                    <span className='war'>Get extra 3% off (price inclusive of discount)</span>
+                                    <span className='hrf'><a href='/'>T&C</a></span>
+                                </div>
+
+                                <div className='omn'>
+                                    <span className='pan'><LocalOfferIcon fontSize='small' /></span>
+                                    <span className='darr'>Special Price</span>
+                                    <span className='war'>Get extra 3% off (price inclusive of discount)</span>
+                                    <span className='hrf'><a href='/'>T&C</a></span>
+                                </div>
+
+                                <div className='omn'>
+                                    <span className='pan'><LocalOfferIcon fontSize='small' /></span>
+                                    <span className='darr'>Special Price</span>
+                                    <span className='war'>Get extra 3% off (price inclusive of discount)</span>
+                                    <span className='hrf'><a href='/'>T&C</a></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='khata'>
+                            <div className='verr'>
+                                <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxOCIgaGVpZ2h0PSIxOCI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48ZWxsaXBzZSBjeD0iOSIgY3k9IjE0LjQ3OCIgZmlsbD0iI0ZGRTExQiIgcng9IjkiIHJ5PSIzLjUyMiIvPjxwYXRoIGZpbGw9IiMyODc0RjAiIGQ9Ik04LjYwOSA3LjAxYy0xLjA4IDAtMS45NTctLjgyNi0xLjk1Ny0xLjg0NSAwLS40ODkuMjA2LS45NTguNTczLTEuMzA0YTIuMDIgMi4wMiAwIDAgMSAxLjM4NC0uNTRjMS4wOCAwIDEuOTU2LjgyNSAxLjk1NiAxLjg0NCAwIC40OS0uMjA2Ljk1OS0uNTczIDEuMzA1cy0uODY0LjU0LTEuMzgzLjU0ek0zLjEzIDUuMTY1YzAgMy44NzQgNS40NzkgOC45MjIgNS40NzkgOC45MjJzNS40NzgtNS4wNDggNS40NzgtOC45MjJDMTQuMDg3IDIuMzEzIDExLjYzNCAwIDguNjA5IDAgNS41ODMgMCAzLjEzIDIuMzEzIDMuMTMgNS4xNjV6Ii8+PC9nPjwvc3ZnPg==" alt="lo" />
+                                <span className='kopl'>Deliver to</span>
+                            </div>
+                        </div>
+
+                        <div className="hyu">
+                            <input type="text" placeholder="Enter delivery pincode" value="" maxlength="6" autocomplete="off" className="cfnctZ" />
+                            <span className="UgLoKgjlIjY">Check</span></div>
+                    </div>
+
+                    <div className='delvi'>
+                        <span className='oil'>Usually delivered in3 days?</span>
+                        <span className='soil'>Enter pincode for exact delivery dates/charges</span>
+                    </div>
+
+                    <div className='selt'>
+                        <div className='seller'>
+                            Seller
+                        </div>
+                        <div className='policy'>
+                            <div className='nam'>
+                                <div className='band'>
+                                    Campusactivewear</div>
+                                <div className='poli'>4.4<StarBorderIcon className='i-st' fontSize='small' /></div>
+                            </div>
+
+                            <div className='san'>
+                                <span className="win">14 Days Return Policy</span>
+                                <span className='jio'>?</span>
+                            </div>
+
+                            <div className='dhan'>
+                                <span className='pik'>
+                                    <a href='/'>View more sellers </a>
+                                </span>
+                                <span className='Sik'>starting from ₹965</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='coin'>
+                        <img src="https://rukminim1.flixcart.com/lockin/763/182/images/CCO__PP_2019-07-14.png?q=50" alt='op' />
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
