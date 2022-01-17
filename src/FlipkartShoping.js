8c8af8bd0cf6e6b192bb62643e448c8cf8a301e9
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2022-01-17 03:38:28 -0800

    init

--- src/FlipkartShoping.js
+++ src/FlipkartShoping.js
@@ -21,8 +21,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/k0flmkw0/key-chain/7/c/a/silver-torch-6-in-1-multitool-heavy-car-stealodeal-original-imafk7nkxxwmtktz.jpeg?q=70"
                     title="StealODeal Silver Torch 6 in 1 Multitool Heavy Car Key"
                     rating="3.5"
-                    price="124"
-                    cut="434"
+                    price={124}
+                    cut={434}
                     offer="65% off"
                    />
 
@@ -31,8 +31,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/kk2wl8w0/key-chain/q/g/r/combo-pack-of-superhero-thor-and-captain-america-shield-keyring-original-imafzge5juqcpd5q.jpeg?q=70"
                     title="ibex Combo Pack of Superhero Thor and Captain America S."
                     rating="3.9"
-                    price="195"
-                    cut="399"
+                    price={195}
+                    cut={399}
                     offer="51% off"
                    />
 
@@ -41,8 +41,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/ki4w0i80-0/key-chain/x/h/y/red-multi-utility-8-in-1-flipkart-smartbuy-original-imafxzuph6yx2zpu.jpeg?q=70"
                     title="Flipkart SmartBuy Red Multi-Utility 8 in 1 Key Chain"
                     rating="3.6"
-                    price="251"
-                    cut="499"
+                    price={251}
+                    cut={499}
                     offer="49% off"
                    />
 
@@ -52,8 +52,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/kj0bp8w0-0/key-chain/s/i/b/genuine-leather-key-chain-kr-106-hidemade-original-imafyzdbgwhgze6r.jpeg?q=70"
                     title="HIDEMADE GENUINE LEATHER KEY CHAIN KR-106 Key Chain"
                     rating="4.2"
-                    price="179"
-                    cut="899"
+                    price={179}
+                    cut={899}
                     offer="80% off"
                    />
                  </div>
@@ -64,8 +64,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/kkmwr680/key-chain/p/g/r/army-keychain-with-torch-screwdriver-knife-bottle-opener-original-imafzxru6gzy4vgb.jpeg?q=70"
                     title="Flipkart SmartBuy Army Keychain with Torch, Screwdriver..."
                     rating="4.2"
-                    price="251"
-                    cut="499"
+                    price={251}
+                    cut={499}
                     offer="49% off"
                    />
 
@@ -74,8 +74,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/k0o69ow0/key-chain/d/y/n/blue-6-in-1-multitool-stealodeal-original-imafkf9tntngvp9t.jpeg?q=70"
                     title="StealODeal Blue 6 in 1 Multitool Key Chain"
                     rating="3.9"
-                    price="211"
-                    cut="499"
+                    price={211}
+                    cut={499}
                     offer="57% off"
                    />
 
@@ -85,8 +85,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/kj0bp8w0-0/key-chain/4/d/5/leather-key-chain-kr-105-hidemade-original-imafyzd5anbfpyg7.jpeg?q=70"
                     title="HIDEMADE LEATHER KEY CHAIN KR-105 Key Chain"
                     rating="4.4"
-                    price="153"
-                    cut="899"
+                    price={153}
+                    cut={899}
                     offer="82% off"
                    />
 
@@ -95,8 +95,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/kgmla4w0/key-chain/d/6/s/drive-safe-exclusive-metal-keychain-for-lovers-best-friends-original-imafwt2uamjmzctx.jpeg?q=70"
                     title="eweft Drive Safe Exclusive Metal Keychain For Lovers\ B.."
                     rating="4.5"
-                    price="152"
-                    cut="299"
+                    price={152}
+                    cut={299}
                     offer="64% off"
                    />
 
@@ -109,8 +109,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/krdtlzk0/key-chain/l/9/q/multi-utility-6-in-1-torch-flipkart-smartbuy-original-imag56zckvvqppxq.jpeg?q=70"
                     title="Flipkart SmartBuy Multi-Utility 6 in 1 Torch Key Chain."
                     rating="4.1"
-                    price="249"
-                    cut="499"
+                    price={249}
+                    cut={499}
                     offer="50% off"
                    />
 
@@ -119,8 +119,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/k3g73bk0/key-chain/j/z/c/black-carabiner-hook-military-keychain-with-torch-screwdriver-original-imafmky7y953fqna.jpeg?q=70"
                     title="Flipkart SmartBuy Black Carabiner Hook Military Keychai."
                     rating="4.1"
-                    price="287"
-                    cut="499"
+                    price={287}
+                    cut={499}
                     offer="42% off"
                    />
 
@@ -130,8 +130,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/k391w280/key-chain/x/s/v/doctor-strange-eye-marvel-avengers-infinity-war-time-stone-original-imafmfhvuweqzfkv.jpeg?q=70"
                     title="MASHKI Doctor Strange Eye Marvel Avengers Infinity War."
                     rating="4.1"
-                    price="142"
-                    cut="376"
+                    price={142}
+                    cut={376}
                     offer="62% off"
                    />
 
@@ -140,8 +140,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/k7jdg280/key-chain/f/r/f/pubg-bullet-kar98-keychain-with-golden-kar98-sniper-new-key-ring-original-imafpr9f94srkzru.jpeg?q=70"
                     title="MASHKI PUBG Bullet & Kar98 Keychain with Golden Kar98 S.."
                     rating="3.8"
-                    price="170"
-                    cut="499"
+                    price={170}
+                    cut={499}
                     offer="65% off"
                    />
 
@@ -154,8 +154,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/k0y6cnk0/key-chain/n/t/4/romantic-kissing-couple-valentine-friendship-day-mashki-original-imafkjbvhpd2d57d.jpeg?q=70"
                     title="Shopiora Romantic Couple Love best Collectible and Gift."
                     rating="4.6"
-                    price="142"
-                    cut="549"
+                    price={142}
+                    cut={549}
                     offer="76% off"
                    />
 
@@ -164,8 +164,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/ksyz8280/key-chain/k/b/e/heart-shaped-metallic-drivesafe-keychain-for-lovers-spouses-original-imag6f7vr4zhctcu.jpeg?q=70"
                     title="eweft Heart Shaped Metallic Drivesafe Keychain For Love."
                     rating="4.4"
-                    price="130"
-                    cut="499"
+                    price={130}
+                    cut={499}
                     offer="73% off"
                    />
 
@@ -175,8 +175,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/kh3qkcw0/key-chain/4/z/q/pubg-awm-gun-keychian-combo-of-2-exclusive-premium-quality-original-imafx6a9ywfjgkhg.jpeg?q=70"
                     title="eweft Pubg AWM Gun Keychian Combo of 2 Exclusive Premium."
                     rating="3.9"
-                    price="195"
-                    cut="499"
+                    price={195}
+                    cut={499}
                     offer="60% off"
                    />
 
@@ -185,8 +185,8 @@ function FlipkartShoping() {
                     image="https://rukminim1.flixcart.com/image/612/612/kvtuxe80/carabiner/h/n/v/fabric-id-tag-with-black-color-ktm-shaded-design-1-avi-original-imag8mxspyzgjbvz.jpeg?q=70"
                     title="AVI Fabric ID tag with Black Color KTM Shaded Design Locking Key. "
                     rating="4.2"
-                    price="110"
-                    cut="349"
+                    price={110}
+                    cut={349}
                     offer="68% off"
                    />
 
