db4084465c01e964eae28c9be7d8a7b59b215da3
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2022-02-08 21:57:28 -0800

    init

---
+++ src/firebases.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7p2EMDf745M5vjsUZV4vqnSBA4Zf8Syg",
+    authDomain: "flipkart-project-319f6.firebaseapp.com",
+    projectId: "flipkart-project-319f6",
+    storageBucket: "flipkart-project-319f6.appspot.com",
+    messagingSenderId: "823628231402",
+    appId: "1:823628231402:web:88afdcc9d2238bed891d93"
+  };
